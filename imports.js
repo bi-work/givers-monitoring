6cf7209e43fc58ad459408d624f25c42466adaa9
Tim <truecarry@gmail.com>
2024-01-30 23:08:20 +0300

    vite, hash stats

--- imports.js
+++ imports.js
@@ -1,5 +1,11 @@
-window.Buffer = require('buffer').Buffer;
-window.TonClient = require('@ton/ton').TonClient;
-window.getHttpEndpoint = require('@orbs-network/ton-access').getHttpEndpoint;
-window.Address = require('@ton/ton').Address;
-window.fromNano = require('@ton/ton').fromNano;
+import { Buffer } from 'buffer'
+window.Buffer = Buffer// require('buffer').Buffer;
+
+const  {TonClient, Address, fromNano} = await import('@ton/ton')
+const {getHttpEndpoint} = await import('@orbs-network/ton-access')
+
+
+window.TonClient =TonClient // require('@ton/ton').TonClient;
+window.getHttpEndpoint = getHttpEndpoint// require('@orbs-network/ton-access').getHttpEndpoint;
+window.Address = Address// require('@ton/ton').Address;
+window.fromNano = fromNano// require('@ton/ton').fromNano;
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+import './imports'
+
+const totalDiff = BigInt('115792089237277217110272752943501742914102634520085823245724998868298727686144')
+const hashrate3080 = BigInt('2000000000')
+
 async function main() {
+    console.log('main')
     const client = new TonClient({
         endpoint: await getHttpEndpoint(),
     });
@@ -8,6 +14,8 @@ async function main() {
         try {
             // Parse the address from the current giver
             const address = Address.parse(giver);
+            const giverAddress = giverAddresses[index]
+            
 
             // Fetch the balance using the parsed address
             const balanceResponse = await client.callGetMethod(
@@ -15,12 +23,25 @@ async function main() {
                 'get_wallet_data',
                 []
             );
+            const powRes = await client.callGetMethod(
+                Address.parse(giverAddress),
+                'get_pow_params',
+                []
+            );
 
             // Read the number from the balance response
             const balance = balanceResponse.stack.readNumber();
 
+            const seed = powRes.stack.readBigNumber()
+            const complexity = powRes.stack.readBigNumber()
+            const iterations = powRes.stack.readBigNumber()
+
+            console.log('com', complexity)
+
             // Format the number with commas
             const formattedBalance = Math.floor(balance / 1e9).toLocaleString();
+            const hashes = totalDiff / complexity
+            const seconds3080 = hashes / hashrate3080
 
             // Update the progress bar for the current giver
             const progress = document.getElementById(`giver${index + 1}`);
@@ -33,7 +54,9 @@ async function main() {
                     ((index % 10) + 1) +
                     ' - Balance: ' +
                     formattedBalance +
-                    ' GRAM';
+                    ' GRAM'
+                    + ' Hashes: ' + formatN(Number(hashes))
+                    + ' Seconds on 3080: ' + seconds3080
             }
 
             return balance; // Return the balance for use in the total
@@ -92,6 +115,24 @@ async function main() {
 }
 
 // Call the function to start the update process
-document.addEventListener('DOMContentLoaded', (event) => {
-    main();
-});
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('event')
+//     main();
+// });
+
+// console.log('main.js')
+main()
+
+function formatN (n) {
+	const unitList = ['y', 'z', 'a', 'f', 'p', 'n', 'u', 'm', '', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+	const zeroIndex = 8;
+	const nn = n.toExponential(2).split(/e/);
+	let u = Math.floor(+nn[1] / 3) + zeroIndex;
+	if (u > unitList.length - 1) {
+		u = unitList.length - 1;
+	} else
+	if (u < 0) {
+		u = 0;
+	}
+	return nn[0] * Math.pow(10, +nn[1] - (u - zeroIndex) * 3) + unitList[u];
+}
