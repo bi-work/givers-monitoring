1ab3de5dfb029c22f61555c8a1d5488a8febe055
Tim <truecarry@gmail.com>
2024-02-21 08:15:35 +0300

    Update code to work with toncenter

--- imports.js
+++ imports.js
@@ -1,11 +1,12 @@
 import { Buffer } from 'buffer'
 window.Buffer = Buffer// require('buffer').Buffer;
 
-const  {TonClient, Address, fromNano} = await import('@ton/ton')
+const  {TonClient,TonClient4, Address, fromNano} = await import('@ton/ton')
 const {getHttpEndpoint} = await import('@orbs-network/ton-access')
 
 
 window.TonClient =TonClient // require('@ton/ton').TonClient;
+window.TonClient4 =TonClient4 // require('@ton/ton').TonClient;
 window.getHttpEndpoint = getHttpEndpoint// require('@orbs-network/ton-access').getHttpEndpoint;
 window.Address = Address// require('@ton/ton').Address;
 window.fromNano = fromNano// require('@ton/ton').fromNano;
--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const hashrate3080 = BigInt('2000000000')
 async function main() {
     console.log('main')
     const client = new TonClient({
-        endpoint: await getHttpEndpoint(),
+        endpoint: 'https://toncenter.com/api/v2/jsonRPC',
+
     });
 
     // Function to update a single giver's balance
@@ -15,19 +16,21 @@ async function main() {
             // Parse the address from the current giver
             const address = Address.parse(giver);
             const giverAddress = giverAddresses[index]
-            
+
 
             // Fetch the balance using the parsed address
-            const balanceResponse = await client.callGetMethod(
+            await delay(1000)
+            const balanceResponse = await CallForSuccess(() => client.callGetMethod(
                 address,
                 'get_wallet_data',
                 []
-            );
-            const powRes = await client.callGetMethod(
+            ), 20, 500);
+            await delay(1000)
+            const powRes = await CallForSuccess(() => client.callGetMethod(
                 Address.parse(giverAddress),
                 'get_pow_params',
                 []
-            );
+            ), 20, 500);
 
             // Read the number from the balance response
             const balance = balanceResponse.stack.readNumber();
@@ -36,7 +39,7 @@ async function main() {
             const complexity = powRes.stack.readBigNumber()
             const iterations = powRes.stack.readBigNumber()
 
-            console.log('com', complexity)
+            console.log('com', giverAddress, complexity)
 
             // Format the number with commas
             const formattedBalance = Math.floor(balance / 1e9).toLocaleString();
@@ -48,6 +51,14 @@ async function main() {
             if (progress) {
                 progress.value = balance; // You may need to adjust based on your scale
                 const label = progress.previousElementSibling; // Assuming label is right before progress bar
+                console.log(label.textContent.split('#')[0] +
+                    '#' +
+                    ((index % 10) + 1) +
+                    ' - Balance: ' +
+                    formattedBalance +
+                    ' GRAM'
+                    + ' Hashes: ' + formatN(Number(hashes))
+                    + ' Seconds on 3080: ' + seconds3080, ' complexity', complexity)
                 label.textContent =
                     label.textContent.split('#')[0] +
                     '#' +
@@ -97,10 +108,16 @@ async function main() {
     // Infinite loop to update balances
     while (true) {
         // Create a promise for each giver's balance update
-        const updatePromises = givers.map(updateGiverBalance);
 
+        // const updatePromises = givers.map(updateGiverBalance);
+
+        let balances = []
+        for (const [giver, i] of givers.entries()) {
+            const b = await updateGiverBalance(i, giver)
+            balances.push(b)
+        }
         // Wait for all updates to complete and sum the balances
-        const balances = await Promise.all(updatePromises);
+        // const balances = await Promise.all(updatePromises);
         const totalBalance = balances.reduce(
             (acc, balance) => acc + balance,
             0
@@ -109,8 +126,8 @@ async function main() {
         // Update the total mining progress bar
         updateTotalMiningProgress(totalBalance);
 
-        // Wait for 10 seconds before starting the next iteration
-        await new Promise((resolve) => setTimeout(resolve, 10000));
+        // Wait for 300 seconds before starting the next iteration
+        await new Promise((resolve) => setTimeout(resolve, 300000));
     }
 }
 
@@ -123,16 +140,49 @@ async function main() {
 // console.log('main.js')
 main()
 
-function formatN (n) {
-	const unitList = ['y', 'z', 'a', 'f', 'p', 'n', 'u', 'm', '', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
-	const zeroIndex = 8;
-	const nn = n.toExponential(2).split(/e/);
-	let u = Math.floor(+nn[1] / 3) + zeroIndex;
-	if (u > unitList.length - 1) {
-		u = unitList.length - 1;
-	} else
-	if (u < 0) {
-		u = 0;
-	}
-	return nn[0] * Math.pow(10, +nn[1] - (u - zeroIndex) * 3) + unitList[u];
+function formatN(n) {
+    const unitList = ['y', 'z', 'a', 'f', 'p', 'n', 'u', 'm', '', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+    const zeroIndex = 8;
+    const nn = n.toExponential(2).split(/e/);
+    let u = Math.floor(+nn[1] / 3) + zeroIndex;
+    if (u > unitList.length - 1) {
+        u = unitList.length - 1;
+    } else
+        if (u < 0) {
+            u = 0;
+        }
+    return nn[0] * Math.pow(10, +nn[1] - (u - zeroIndex) * 3) + unitList[u];
+}
+
+export async function CallForSuccess(
+    toCall,
+    attempts = 20,
+    delayMs = 100
+) {
+    if (typeof toCall !== 'function') {
+        throw new Error('unknown input')
+    }
+
+    let i = 0
+    let lastError
+
+    while (i < attempts) {
+        try {
+            const res = await toCall()
+            return res
+        } catch (err) {
+            lastError = err
+            i++
+            await delay(delayMs)
+        }
+    }
+
+    console.log('error after attempts', i)
+    throw lastError
+}
+
+export function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
 }
