355da4791edbd4d62c697fdcb6dede8a0e598525
bi-work <bi_work@mail.ru>
2024-05-20 02:10:37 +0500

    givers_gpu.js

---
+++ givers_gpu.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.givers100000 = exports.givers10000 = exports.givers1000 = void 0;
+exports.givers1000 = [
+    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZ', reward: 1000 }, // 1 000
+];
+exports.givers10000 = [
+    { address: 'ZZZZZZZZ', reward: 10000 },
+    { address: 'ZZZZZZZZ', reward: 10000 },
+    { address: 'ZZZZZZZZ', reward: 10000 },
+    { address: 'ZZZZZZZZ', reward: 10000 },
+    { address: 'ZZZZZZZZ', reward: 10000 },
+    { address: 'ZZZZZZZZ', reward: 10000 },
+    { address: 'ZZZZZZZZ', reward: 10000 },
+    { address: 'ZZZZZZZZ', reward: 10000 },
+    { address: 'ZZZZZZZZ', reward: 10000 },
+    { address: 'ZZZZZZZZ', reward: 10000 }, // 10 000
+];
+exports.givers100000 = [
+    { address: 'ZZZZZZZZ', reward: 100000 },
+    { address: 'ZZZZZZZZ', reward: 100000 },
+    { address: 'ZZZZZZZZ', reward: 100000 },
+    { address: 'ZZZZZZZZ', reward: 100000 },
+    { address: 'ZZZZZZZZ', reward: 100000 },
+    { address: 'ZZZZZZZZ', reward: 100000 },
+    { address: 'ZZZZZZZZ', reward: 100000 },
+    { address: 'ZZZZZZZZ', reward: 100000 },
+    { address: 'ZZZZZZZZ', reward: 100000 },
+    { address: 'ZZZZZZZZ', reward: 100000 }, // 100 000
+];
