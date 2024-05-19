ae06a6b6ece6d0c042685a655f4f88db9a868dcf
bi-work <bi_work@mail.ru>
2024-05-20 03:20:22 +0500

    Update givers_gpu.js

--- givers_gpu.js
+++ givers_gpu.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.givers100000 = exports.givers10000 = exports.givers1000 = void 0;
 exports.givers1000 = [
-    { address: 'ZZZZZZZZ', reward: 1000 },
+    { address: 'ZZZZZZZZZZZZZZZZ', reward: 1000 },
     { address: 'ZZZZZZZZ', reward: 1000 },
     { address: 'ZZZZZZZZ', reward: 1000 },
     { address: 'ZZZZZZZZ', reward: 1000 },
