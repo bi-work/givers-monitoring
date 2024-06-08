56ac85aa6a82f32f5260e4d1cc4565d637ff8f67
bi-work <bi_work@mail.ru>
2024-06-08 11:36:31 +0500

    Update givers_gpu.js

--- givers_gpu.js
+++ givers_gpu.js
@@ -1,12 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.givers100000 = exports.givers10000 = exports.givers1000 = void 0;
+exports.givers100000 = exports.givers10000 = exports.givers1000 = exports.givers200 = exports.givers100 = void 0;
+exports.givers100 = [
+    { address: 'ZZZZZZZZ', reward: 100 },
+    { address: 'ZZZZZZZZ', reward: 100},
+    { address: 'ZZZZZZZZ', reward: 100 },
+    { address: 'ZZZZZZZZ', reward: 100 },
+    { address: 'ZZZZZZZZ', reward: 100 }, // 100
+];
+exports.givers200 = [
+    { address: 'ZZZZZZZZ', reward: 200 },
+    { address: 'ZZZZZZZZ', reward: 200 },
+    { address: 'ZZZZZZZZ', reward: 200 },
+    { address: 'ZZZZZZZZ', reward: 200 },
+    { address: 'ZZZZZZZZ', reward: 200 }, // 200
+];
 exports.givers1000 = [
-    { address: 'ZZZZZZZZZZZZZZZZ', reward: 1000 },
-    { address: 'ZZZZZZZZ', reward: 1000 },
-    { address: 'ZZZZZZZZ', reward: 1000 },
-    { address: 'ZZZZZZZZ', reward: 1000 },
-    { address: 'ZZZZZZZZ', reward: 1000 },
     { address: 'ZZZZZZZZ', reward: 1000 },
     { address: 'ZZZZZZZZ', reward: 1000 },
     { address: 'ZZZZZZZZ', reward: 1000 },
@@ -18,19 +27,9 @@ exports.givers10000 = [
     { address: 'ZZZZZZZZ', reward: 10000 },
     { address: 'ZZZZZZZZ', reward: 10000 },
     { address: 'ZZZZZZZZ', reward: 10000 },
-    { address: 'ZZZZZZZZ', reward: 10000 },
-    { address: 'ZZZZZZZZ', reward: 10000 },
-    { address: 'ZZZZZZZZ', reward: 10000 },
-    { address: 'ZZZZZZZZ', reward: 10000 },
-    { address: 'ZZZZZZZZ', reward: 10000 },
     { address: 'ZZZZZZZZ', reward: 10000 }, // 10 000
 ];
 exports.givers100000 = [
-    { address: 'EQCklqxLokjCHkujRzAZMlaGM4JSUHS3IpUhqQ6y8mzD2DiL', reward: 100000 },
-    { address: 'ZZZZZZZZ', reward: 100000 },
-    { address: 'ZZZZZZZZ', reward: 100000 },
-    { address: 'ZZZZZZZZ', reward: 100000 },
-    { address: 'ZZZZZZZZ', reward: 100000 },
     { address: 'ZZZZZZZZ', reward: 100000 },
     { address: 'ZZZZZZZZ', reward: 100000 },
     { address: 'ZZZZZZZZ', reward: 100000 },
